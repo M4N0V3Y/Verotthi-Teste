179186a42a4301cd1cbff5e7adccc459466487bb
JOSMAR AUGUSTO FONSECA BARBOSA <barbosajaf@gmail.com>
2021-06-29 19:36:29 -0300

    Disponibilizando o material do teste
    Disponibilizando o material do teste

---
+++ Test Software Developer C_C++_C#_NP_New Features/Problem 2/Problem2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        // Complete the circularArrayRotation function below.
+        //a:            an array of integers to rotate
+        //k:            an integer, the rotation count
+        //queries:      an array of integers, the indices to report
+        static int[] circularArrayRotation(int[] a, int k, int[] queries)
+        {
+            int[] shiftA = new int[a.Count()];
+            int[] resultQuery = new int[queries.Count()];
+
+            for (int index = 0; index < a.Length; index++)
+            {
+                shiftA[(index + a.Length + k % a.Length) % a.Length] = a[index];
+            }
+
+            a = shiftA;
+
+            for (int index = 0; index < queries.Length; index++)
+            {
+                resultQuery[index] = a[queries[index]];
+            }
+
+            return resultQuery;
+
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter("OUTPUT2.TXT", true);
+
+            string[] nkq = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nkq[0]);
+
+            int k = Convert.ToInt32(nkq[1]);
+
+            int q = Convert.ToInt32(nkq[2]);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+            ;
+
+            int[] queries = new int[q];
+
+            for (int i = 0; i < q; i++)
+            {
+                int queriesItem = Convert.ToInt32(Console.ReadLine());
+                queries[i] = queriesItem;
+            }
+
+            int[] result = circularArrayRotation(a, k, queries);
+
+            textWriter.WriteLine(string.Join("\n", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+    }
+}
---
+++ Test Software Developer C_C++_C#_NP_New Features/Problem 3/Problem3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace Problem3
+{
+    class Result
+    {
+
+        // Complete the 'pickingNumbers' function below.
+
+        public static int pickingNumbers(List<int> a)
+        {
+            int previous_counter = a.Count();
+            int couter = 0;
+            int bigest_member = a.Max();
+
+            while (a.Count() > 0)
+            {
+                bool toRemove = false;
+                foreach (int value in a)
+                    if ((bigest_member-value) > 2) toRemove = true;   
+
+                if (toRemove){
+                    a.Remove(bigest_member);
+                    bigest_member = a.Max();
+                    couter++;}
+            }
+
+            return (previous_counter-couter);
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter("OUTPUT3.TXT", true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int result = Result.pickingNumbers(a);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
